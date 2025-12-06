92d13de5c83037a50b759483aec12c896ddf2b71
tahamahmoud7097-wq <tahamahmoud7097@gmail.com>
2025-12-06 16:12:21 +0200

    Optimized `Ctx<E>` and its methods even further as well as adding new methods for better dynamic error support that allocate `String`s

--- src/res_ext_methods/with_context.rs
+++ src/res_ext_methods/with_context.rs
@@ -1,25 +1,28 @@
-use std::borrow::Cow;
-
-use crate::Ctx;
+use crate::{Ctx, ctx::CtxMsg};
 
 pub(crate) fn with_context_impl<T, E, F>(res: Result<T, E>, closure: F) -> Result<T, Ctx<E>>
 where
-    F: Fn(&E) -> String,
+    F: FnOnce() -> String,
 {
-    res.map_err(|e| {
-        let message = Cow::Owned(closure(&e));
-        let mut ctx = Ctx::new(e);
-        ctx.msg.push(message);
-        ctx
-    })
+    match res {
+        Ok(ok) => Ok(ok),
+        Err(e) => {
+            let mut ctx = Ctx::new(e);
+            ctx.msg.push(CtxMsg::Dynamic(closure()));
+            Err(ctx)
+        }
+    }
 }
 
 pub(crate) fn extra_with_ctx_impl<T, E, F>(res: Result<T, Ctx<E>>, closure: F) -> Result<T, Ctx<E>>
 where
-    F: Fn(&E) -> String,
+    F: FnOnce() -> String,
 {
-    res.map_err(|mut e| {
-        e.msg.push(Cow::Owned(closure(&e.source)));
-        e
-    })
+    match res {
+        Ok(ok) => Ok(ok),
+        Err(mut e) => {
+            e.msg.push(CtxMsg::Dynamic(closure()));
+            Err(e)
+        }
+    }
 }
