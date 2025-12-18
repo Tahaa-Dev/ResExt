e93d0798ca1632a2a0f14005e245efa6cf198d33
tahamahmoud7097-wq <tahamahmoud7097@gmail.com>
2025-12-19 01:50:18 +0200

    Optimized `Ctx<E>` and its methods by making it store a Vec<u8> of bytes of message(s) instead of actual bytes and added more methods

--- src/res_ext_methods/context.rs
+++ src/res_ext_methods/context.rs
@@ -1,13 +1,12 @@
-use crate::{Ctx, ctx::CtxMsg};
+use crate::Ctx;
 
 pub(crate) fn new_context_impl<T, E>(res: Result<T, E>, msg: &'static str) -> Result<T, Ctx<E>> {
     match res {
         Ok(ok) => Ok(ok),
-        Err(e) => {
-            let mut ctx = Ctx::new(e);
-            ctx.msg.push(CtxMsg::Static(msg));
-            Err(ctx)
-        }
+        Err(e) => Err(Ctx {
+            msg: msg.as_bytes().to_vec(),
+            source: e,
+        }),
     }
 }
 
@@ -15,7 +14,32 @@ pub(crate) fn extra_ctx_impl<T, E>(res: Result<T, Ctx<E>>, msg: &'static str) ->
     match res {
         Ok(ok) => Ok(ok),
         Err(mut e) => {
-            e.msg.push(CtxMsg::Static(msg));
+            e.msg.push(b'\n');
+            e.msg.extend_from_slice(msg.as_bytes());
+            Err(e)
+        }
+    }
+}
+
+pub(crate) fn byte_context_impl<T, E>(res: Result<T, E>, bytes: &[u8]) -> Result<T, Ctx<E>> {
+    match res {
+        Ok(ok) => Ok(ok),
+        Err(e) => Err(Ctx {
+            msg: bytes.to_vec(),
+            source: e,
+        }),
+    }
+}
+
+pub(crate) fn extra_byte_context_impl<T, E>(
+    res: Result<T, Ctx<E>>,
+    bytes: &[u8],
+) -> Result<T, Ctx<E>> {
+    match res {
+        Ok(ok) => Ok(ok),
+        Err(mut e) => {
+            e.msg.push(b'\n');
+            e.msg.extend_from_slice(bytes);
             Err(e)
         }
     }
--- tests/test.rs
+++ tests/test.rs
@@ -1,13 +1,1 @@
-use resext::*;
 
-fn integration_tests() {
-    let error: Result<&str, std::io::Error> = Err(std::io::Error::other("Error"));
-
-    let ok: Result<&str, std::io::Error> = Ok("World!");
-
-    let error_with_ctx = error.context("I/O Error").better_expect("Error", 1, true);
-
-    let ok_to_value = ok.or_exit(1);
-
-    println!("{}\n{}", ok_to_value, error_with_ctx);
-}
